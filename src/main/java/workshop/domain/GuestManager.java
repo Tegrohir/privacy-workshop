9f883ceeb2e92cd1b83c935527ab8c45cafd9047
Wesley de Kraker <github@wesleydekraker.com>
2018-06-01 16:47:31 +0200

    Replaced custom cookie with default Java session

--- src/main/java/workshop/domain/GuestManager.java
+++ src/main/java/workshop/domain/GuestManager.java
@@ -1,28 +1,12 @@
 package workshop.domain;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 public class GuestManager {
     public static Guest getGuest(HttpServletRequest request) {
         Service service = Service.getInstance();        
-        String session = null;
-        Guest guest = null;
         
-        if (request.getCookies() == null) {
-            return null;
-        }
-        
-        for (Cookie cookie: request.getCookies()) {
-            if (cookie.getName().equals("session")) {
-                session = cookie.getValue();
-            }
-        }
-        
-        if (session != null) {
-            guest = service.getGuest(session);
-        }
-        
-        return guest;
+        String session = request.getSession().getId();        
+        return service.getGuest(session);
     }
 }
--- src/main/java/workshop/view/StartServlet.java
+++ src/main/java/workshop/view/StartServlet.java
@@ -1,11 +1,9 @@
 package workshop.view;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,12 +26,9 @@ public class StartServlet extends HttpServlet {
     
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
         if (GuestManager.getGuest(request) == null) {
-            String session = UUID.randomUUID().toString();
+            String session = request.getSession().getId();
             Guest guest = new Guest();
             guest.setSession(session);
-            
-            Cookie cookie = new Cookie("session", session);
-            response.addCookie(cookie);
             
             Service service = Service.getInstance();
             service.addGuest(guest);
